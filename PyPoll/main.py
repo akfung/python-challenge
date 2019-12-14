4942153c242b93a64e56db6960b6309599bbb1b3
Amos Fung <amosfung@dhcp-v104-042.mobile.uci.edu>
2019-12-14 12:22:49 -0800

    count votes and generate popular vote

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,8 +2,10 @@
 import csv
 import os
 
-vote_count = 0
+total_vote_count = 0
 candidate_list = []
+vote_per_candidate = []
+popular_vote = []
 
 #look out its the csv caravan coming to take our jobs
 csv_path = os.path.join(".", "election_data.csv")
@@ -14,9 +16,23 @@ with open(csv_path, newline='', encoding="UTF-8") as csv_file:
     next(csv_reader)
     for row in csv_reader:
         #count the number of voters 
-        vote_count += 1
+        total_vote_count += 1
         #if statement to add cadidates to a list if they haven't come up yet or aren't named andrew yang
         if not(row[2] in candidate_list):
             candidate_list.append(row[2])
+            #add a 0 entry to list for tracking that candidate's votes
+            vote_per_candidate.append(0)
+        #add +1 to that candidate's vote total that tracks with their index 
+        vote_per_candidate[candidate_list.index(row[2])] +=1 
+    #divide vote per candidate by total vote to find popular vote because fuck me if I'm gonna calculate electoral votes
+    for vote in vote_per_candidate:
+        popular_vote.append(round(vote*100/total_vote_count, 4))
+        #make array connecting candidate to votes
+        vote_array = zip(candidate_list, vote_per_candidate)
+        vote_array.sort(key=sortSecond)
     print(candidate_list)
-    print(vote_count)
+    print(total_vote_count)
+    print(vote_per_candidate)
+    print(popular_vote)
+    print(vote_array)
+    
